eb22c14ac760f7f3c55c6616ded7e3e88affdb42
Daniel Mints <dannymints@gmail.com>
2025-06-15 08:46:55 -0700

    Initial Commit

---
+++ Hiking Assistant/Hiking_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hiking_AssistantApp.swift
+//  Hiking Assistant
+//
+//  Created by Daniel on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Hiking_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hiking AssistantTests/Hiking_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hiking_AssistantTests.swift
+//  Hiking AssistantTests
+//
+//  Created by Daniel on 6/15/25.
+//
+
+import Testing
+@testable import Hiking_Assistant
+
+struct Hiking_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
